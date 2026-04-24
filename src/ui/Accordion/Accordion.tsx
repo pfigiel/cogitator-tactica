c8af48fb7522d94791157f135d7f5210707f4790
Paweł Figiel <pawel.figiel8@gmail.com>
2026-04-24 22:41:29 +0200

    fix: fix accordion compound components visibility

--- src/ui/Accordion/Accordion.tsx
+++ src/ui/Accordion/Accordion.tsx
@@ -1,3 +1,20 @@
-import { Accordion as MantineAccordion } from "@mantine/core";
+import { Accordion as MantineAccordion, AccordionProps } from "@mantine/core";
+import styles from "./Accordion.module.css";
 
-export const Accordion = MantineAccordion;
+const AccordionRoot = ({ classNames, ...props }: AccordionProps) => (
+  <MantineAccordion
+    classNames={{
+      item: styles.item,
+      control: styles.control,
+      ...classNames,
+    }}
+    {...props}
+  />
+);
+
+export const Accordion = Object.assign(AccordionRoot, {
+  Item: MantineAccordion.Item,
+  Control: MantineAccordion.Control,
+  Panel: MantineAccordion.Panel,
+  Chevron: MantineAccordion.Chevron,
+});
