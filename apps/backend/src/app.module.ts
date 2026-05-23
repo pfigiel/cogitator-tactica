fc6e78e7748aa135ab8ec69c14499d2ed3b64b02
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-23 12:57:03 +0200

    feat: remove embeddings module from app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { EmbeddingsModule } from "./embeddings/embeddings.module";
 import { HealthModule } from "./health/health.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    HealthModule,
-    EmbeddingsModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule],
 })
 export class AppModule {}
