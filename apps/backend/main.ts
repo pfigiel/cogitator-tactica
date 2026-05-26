b1e40e740202d9b5ab8d2e567a0c551a9986595a
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-26 14:44:45 +0200

    feat: add class-validator and ValidationPipe

--- apps/backend/main.ts
+++ apps/backend/main.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./src/app.module";
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3001);
 };
 
