e5a6035b53bfd6ba6a71e971c8840952db539d52
Paweł Figiel <pawel.figiel8@gmail.com>
2026-05-27 11:31:02 +0200

    feat: add api barrel and handlers index

---
+++ apps/web/src/api/tests/handlers/index.ts
@@ -0,0 +1,5 @@
+import { units } from "./units";
+import { parsePrompt } from "./parsePrompt";
+import { calculate } from "./calculate";
+
+export const handlers = { units, parsePrompt, calculate };
